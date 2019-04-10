f2a48b72c5fbb956df9d4f30f2266bebc5150f22
Nur Muhammad Luthfi <luthfi2444@gmail.com>
2019-04-10 19:36:14 +0700

    add class makanan

---
+++ app/src/main/java/com/example/luthfi/motionstudygroup2fix/model/Makanan.java
@@ -0,0 +1,28 @@
+package com.example.luthfi.motionstudygroup2fix.model;
+
+public class Makanan {
+
+    private String title;
+    private float price;
+
+    public Makanan(String title, float price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
